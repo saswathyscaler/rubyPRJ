073504521bbbd563d5bdc4d92e379da7e8e76083
saswat <saswatr.mohanty@hyscaler.com>
2023-09-07 10:36:53 +0530

    crud

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,53 @@
 class StudentsController < ApplicationController
   def index
+    @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+      
   end
 
   def edit
+    @student = Student.find(params[:id])
+
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(studetn_params)
+    redirect_to students_path
+    
+
+    
+  end
+
+
+  def create
+    student = Student.create(studetn_params)
+    redirect_to students_path
+    
   end
 
   def new
+    @student =Student.new
   end
+
+
+def destroy
+  @student = Student.find(params[:id])
+  @student.destroy
+redirect_to students_path
+  
+end
+
+
+
+  private
+
+
+  def studetn_params
+    params.require(:student).permit(:name,:age)
+  end
+
 end
